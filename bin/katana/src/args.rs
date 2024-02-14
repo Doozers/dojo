f06bf50694a985d9137f8e7a4715e1e53bb626b8
Tarrence van As <tarrence@vanas.family>
2024-02-14 16:53:20 -0500

    Add torii to default katana rpc

--- bin/katana/src/args.rs
+++ bin/katana/src/args.rs
@@ -212,7 +212,7 @@ impl KatanaArgs {
     }
 
     pub fn server_config(&self) -> ServerConfig {
-        let mut apis = vec![ApiKind::Starknet];
+        let mut apis = vec![ApiKind::Starknet, ApiKind::Torii];
         // only enable `katana` API in dev mode
         if self.dev {
             apis.push(ApiKind::Katana);
